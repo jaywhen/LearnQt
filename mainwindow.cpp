52eb2eb8e917c7720ee008777269c257bad9697f
jaywhen <jaywhen>
2020-03-16 23:01:58 +0800

    The framework is written

---
+++ mainwindow.cpp
@@ -0,0 +1,122 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    setWindowTitle("XClc");
+    ui->lineEdit->setText("0");
+    ifClickedOperator = false;
+    opFlag = 0;
+    currentEcho = "";
+    tmpStorage = "";
+    result = 0;
+    echoResult = "";
+
+
+}
+
+
+void MainWindow::on_pushButton_AC_clicked()
+{
+    ui->lineEdit->setText("0");
+    ifClickedOperator = false;
+    opFlag = 0;
+    currentEcho = "";
+    tmpStorage = "";
+    result = 0;
+    echoResult = "";
+
+}
+void MainWindow::on_pushButton_0_clicked()
+{
+    ui->lineEdit->setText("0");
+
+}
+void MainWindow::on_pushButton_1_clicked()
+{
+    ui->lineEdit->setText("1");
+
+
+}
+void MainWindow::on_pushButton_2_clicked()
+{
+    ui->lineEdit->setText("2");
+
+
+}
+void MainWindow::on_pushButton_3_clicked()
+{
+    ui->lineEdit->setText("3");
+
+
+}
+void MainWindow::on_pushButton_4_clicked()
+{
+    ui->lineEdit->setText("4");
+
+
+}
+void MainWindow::on_pushButton_5_clicked()
+{
+    ui->lineEdit->setText("5");
+
+
+}
+void MainWindow::on_pushButton_6_clicked()
+{
+    ui->lineEdit->setText("6");
+
+
+}
+void MainWindow::on_pushButton_7_clicked()
+{
+    ui->lineEdit->setText("7");
+
+
+}
+void MainWindow::on_pushButton_8_clicked()
+{
+    ui->lineEdit->setText("8");
+
+
+}
+void MainWindow::on_pushButton_9_clicked()
+{
+    ui->lineEdit->setText("9");
+
+
+}
+
+void MainWindow::on_pushButton_add_clicked()
+{
+    ui->lineEdit->setText("+");
+
+
+}
+void MainWindow::on_pushButton_sub_clicked()
+{
+    ui->lineEdit->setText("-");
+}
+void MainWindow::on_pushButton_divi_clicked()
+{
+    ui->lineEdit->setText("÷");
+}
+void MainWindow::on_pushButton_mult_clicked()
+{
+    ui->lineEdit->setText("x");
+}
+void MainWindow::on_pushButton_equal_clicked()
+{
+    ui->lineEdit->setText("=");
+}
+void MainWindow::on_pushButton_dot_clicked()
+{
+    ui->lineEdit->setText(".");
+}
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
---
+++ mainwindow.h
@@ -0,0 +1,53 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+private slots:
+
+    //operation button
+    void on_pushButton_AC_clicked();
+    void on_pushButton_add_clicked();
+    void on_pushButton_sub_clicked();
+    void on_pushButton_mult_clicked();
+    void on_pushButton_divi_clicked();
+    void on_pushButton_dot_clicked();
+    void on_pushButton_equal_clicked();
+    //numeric button
+    void on_pushButton_0_clicked();
+    void on_pushButton_1_clicked();
+    void on_pushButton_2_clicked();
+    void on_pushButton_3_clicked();
+    void on_pushButton_4_clicked();
+    void on_pushButton_5_clicked();
+    void on_pushButton_6_clicked();
+    void on_pushButton_7_clicked();
+    void on_pushButton_8_clicked();
+    void on_pushButton_9_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    bool ifClickedOperator; //mark whether the operator is clicked
+    int opFlag; //represents which operator is selected
+
+    QString currentEcho; //
+    QString tmpStorage;
+
+    double result;
+    QString echoResult;
+
+
+
+};
+#endif // MAINWINDOW_H
